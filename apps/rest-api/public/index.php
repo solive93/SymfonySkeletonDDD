3c9a1f333e73cf9c8977f9349e5dc1f46b562a36
Sergi Oliveres <sergi.oliveres@navigaglobal.com>
2023-01-31 19:16:47 +0100

    chore: fine tune project structure and framework config

--- apps/rest-api/public/index.php
+++ apps/rest-api/public/index.php
@@ -2,7 +2,7 @@
 
 use Company\RestApi\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once __DIR__ . '/../../../vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
